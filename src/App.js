bc560bd2f3530856dad11aeae1277d67678eec3d
acalvom <andreac.3108@gmail.com>
2021-05-11 11:27:11 +0200

    Create user form and add new user

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
-import { v4 as uuidv4 } from 'uuid';
+import {v4 as uuidv4} from 'uuid';
 import UserTable from "./components/UserTable";
+import UserForm from "./components/UserForm";
 
 function App() {
 
@@ -13,12 +14,22 @@ function App() {
     // Como queremos que la tabla tenga valores ya por defecto, necesito añadirlo al estado inicial
     const [users, setUsers] = useState(usersData)
 
+    const addUser = (newUser) => { // Como en las listas
+        newUser.id = uuidv4();
+        setUsers([
+            ...users,
+            newUser
+        ])
+    }
+
     return (
         <div className="container">
             <h1>CRUD App with Hooks</h1>
             <div className="flex-row">
                 <div className="flex-large">
                     <h2>Add user</h2>
+                    {/*Los componentes pueden enviar funciones en sus props, pero no deben llevar los paréntesis*/}
+                    <UserForm addUser={addUser}/>
                 </div>
                 <div className="flex-large">
                     <h2>View users</h2>
---
+++ src/components/UserForm.js
@@ -0,0 +1,46 @@
+import React, {Fragment} from 'react'
+import {useForm} from 'react-hook-form'
+
+const UserForm = (props) => {
+
+    const {register, formState: {errors}, handleSubmit} = useForm();
+    const sendForm = (formData, event) => { //Esta data es la que va a App, a la función addUser que le añade el id
+        event.target.reset();
+        props.addUser(formData);
+    }
+
+    return (
+        <Fragment>
+            <form onSubmit={handleSubmit(sendForm)}>
+                <div className="row">
+                    <div className="col-md-3">
+                        <input className="form-control"
+                               placeholder="Avatar"
+                               {...register("avatar")}/>
+                    </div>
+                    <div className="col-md-3">
+                        <input className="form-control"
+                               placeholder="Name"
+                               {...register("name", {required: true, maxLength: 20})}/>
+                        {errors.name && errors.name.type === "required" && <span>Campo obligatorio</span>}
+                        {errors.name && errors.name.type === "maxLength" && <span>Max 20 caracteres</span>}
+                    </div>
+                    <div className="col-md-3">
+                        <input className="form-control"
+                               placeholder="Age"
+                               {...register("age", {required: true, min: 18, max: 99})}/>
+                        {errors.age && errors.age.type === "required" && <span>Campo obligatorio</span>}
+                        {errors.age && errors.age.type === "min" && <span>You need to an adult</span>}
+                        {errors.age && errors.age.type === "max" && <span>WTF???</span>}
+                    </div>
+                    <div className="col-md-3">
+                        <button type="submit" className="btn btn-primary">Añadir</button>
+                    </div>
+                </div>
+            </form>
+
+        </Fragment>
+    );
+};
+
+export default UserForm;
