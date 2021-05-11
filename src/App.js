5e2fa9bb1c5288e7e732fe9098e6ce52b34495cd
acalvom <andreac.3108@gmail.com>
2021-05-11 08:59:47 +0200

    Create a component with a table to list users

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from "react";
+import UserTable from "./components/UserTable";
 
 function App() {
-  return (
-      <div> Start the project</div>
-  );
+    return (
+        <div className="container">
+            <h1>CRUD App with Hooks</h1>
+            <div className="flex-row">
+                <div className="flex-large">
+                    <h2>Add user</h2>
+                </div>
+                <div className="flex-large">
+                    <h2>View users</h2>
+                    <UserTable/>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
